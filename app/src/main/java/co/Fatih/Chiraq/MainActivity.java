df10d046e3a3c9e34f46ed668bc0a9f3de2c4d23
adasdas <lollol61@live.de>
2018-03-16 13:01:37 +0100

    chatsdk no errors

--- app/src/main/java/co/Fatih/Chiraq/MainActivity.java
+++ app/src/main/java/co/Fatih/Chiraq/MainActivity.java
@@ -1,10 +1,20 @@
 package co.Fatih.Chiraq;
 
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import co.chatsdk.core.session.ChatSDK;
+import co.chatsdk.core.session.Configuration;
+import co.chatsdk.firebase.FirebaseModule;
+import co.chatsdk.firebase.file_storage.FirebaseFileStorageModule;
+
+import co.chatsdk.ui.manager.UserInterfaceModule;
+
 
 
 public class MainActivity extends Activity {
@@ -18,6 +28,26 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         initializeUI();
+        Context context = getApplicationContext();
+
+// Create a new configuration
+        Configuration.Builder builder = new Configuration.Builder(context);
+
+// Perform any configuration steps (optional)
+        builder.firebaseRootPath("prod");
+
+// Initialize the Chat SDK
+        ChatSDK.initialize(builder.build());
+        UserInterfaceModule.activate(context);
+
+// Activate the Firebase module
+        FirebaseModule.activate();
+
+// File storage is needed for profile image upload and image messages
+        FirebaseFileStorageModule.activate();
+
+// Activate any other modules you need.
+// ...
 
     }
 
